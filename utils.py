bf4c2158b49d4a91b16d6b157df65015d43a563d
Thomas Rochefort-Beaudoin <47933584+ThomasRochefortB@users.noreply.github.com>
2023-04-05 14:50:11 -0400

    Added train_model function

--- utils.py
+++ utils.py
@@ -39,3 +39,40 @@ class CustomMNIST(torchvision.datasets.MNIST):
         new_size_h = random.randint(28, 56)
         img = transforms.Resize((new_size_h,new_size_w))(img)
         return img
+    
+def train_model(model, train_loader, test_loader, criterion, optimizer, num_epochs, train_log_freq = 100):
+    # Train model and track the loss as well as the training accuracy:
+    # Set device
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss_train = 0.0
+        running_acc_train = 0.0
+        for i, (images, labels) in enumerate(train_loader):
+            images = [img.to(device) for img in images]
+            labels = labels.to(device) # Move to device
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss_train += loss.item()
+            running_acc_train += (outputs.argmax(1) == labels).float().mean()
+            if i % train_log_freq == 0 and i > 0:
+                print(f"Epoch {epoch} - Batch {i} - Loss: {running_loss_train/(i):.4f} - Accuracy: {running_acc_train/(i):.4f}")
+        #Test the accuracy on the test set:
+        model.eval()
+        with torch.no_grad():
+            running_loss_test = 0.0
+            running_acc_test = 0.0
+            for i, (images, labels) in enumerate(test_loader):
+                images = [img.to(device) for img in images]
+                labels = labels.to(device)
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                running_loss_test += loss.item()
+                running_acc_test += (outputs.argmax(1) == labels).float().mean()
+            print(f"Test - Loss: {running_loss_test/(i):.4f} - Accuracy: {running_acc_test/(i):.4f}")
