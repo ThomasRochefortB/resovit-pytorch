c1706c95a101877528ac226b280692a85dcaa58f
ThomasRochefortB <thomas.rochefort.beaudoin@gmail.com>
2023-04-10 16:48:46 -0400

    finally have a 2d pos embed that makes sense

--- resovit.py
+++ resovit.py
@@ -23,8 +23,7 @@ class PatchEmbedding(nn.Module):
         self.max_length = max_length
         self.linear = nn.Linear(patch_size*patch_size*img_channels, emb_dim)
     
-    
-    def forward(self,x):
+    def to_patch(self,x):
         img_dim = x.shape[-3:]
         
         # Get the number of patches required to cover the image:
@@ -49,9 +48,13 @@ class PatchEmbedding(nn.Module):
         attention_mask = torch.ones(x.shape[0], x.shape[2]//self.patch_size, x.shape[3]//self.patch_size)
         if pad_h > 0 or pad_w > 0:
             attention_mask = F.pad(attention_mask, (0, pad_w//self.patch_size, 0, pad_h//self.patch_size), value=0)
-
         # Flatten the attention mask
         attention_mask = attention_mask.view(x.shape[0], -1)
+
+        # Get the relative x and y position of each patch:
+        y_pos = torch.arange(0, x.shape[2], self.patch_size) / (x.shape[2] - 1)
+        x_pos = torch.arange(0, x.shape[3], self.patch_size) / (x.shape[3] - 1)
+        
 
         x = x.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size) 
         x = x.reshape(x.shape[0], x.shape[1], x.shape[2]*x.shape[3], x.shape[4], x.shape[5])
@@ -61,22 +64,31 @@ class PatchEmbedding(nn.Module):
         if x.shape[1] < self.max_length:
             x = F.pad(x, (0, 0, 0, self.max_length - x.shape[1]), value=0)
             attention_mask = F.pad(attention_mask, (0, self.max_length - attention_mask.shape[1]), value=0)
+
+        #pad y_pos and x_pos with -1 so that they have the same dimension as the attention mask:
+        y_pos = F.pad(y_pos, (0, self.max_length - y_pos.shape[0]), value=-1)
+        x_pos = F.pad(x_pos, (0, self.max_length - x_pos.shape[0]), value=-1)
+
+        return x, attention_mask, x_pos, y_pos
+
+    
+    def forward(self,x):
+        x, attention_mask,x_pos,y_pos = self.to_patch(x)
         x = self.linear(x)        
-        return x, attention_mask
+        return x, attention_mask, x_pos, y_pos
     
 
 class PositionalEmbedding(nn.Module):
     """Adds positional embedding to the input tensor.
     """
 
-    def __init__(self, emb_dim=32, max_length=256):
+    def __init__(self, emb_dim=32, max_length=256 ):
         super().__init__()
         self.emb_dim = emb_dim
         self.max_length = max_length
         self.pos_emb = nn.Parameter(torch.rand(1, max_length, emb_dim))
-
-    def forward(self, x):
-        x = x + self.pos_emb
+    def forward(self, x,x_pos, y_pos):
+        x = x + self.pos_emb*x_pos.unsqueeze(0).unsqueeze(-1).expand(-1, -1, self.emb_dim) + self.pos_emb*y_pos.unsqueeze(0).unsqueeze(-1).expand(-1, -1, self.emb_dim)
         return x
     
 
@@ -96,19 +108,18 @@ class ResoVit(nn.Module):
                 num_classes=10,
                 img_channels=1): 
         super().__init__() # don't forget the super().__init__()!
-
-        self.patch_embedding = PatchEmbedding(patch_size=patch_size,max_length=max_length, emb_dim=embedding_dim,img_channels=img_channels)
+        self.embedding_dim = embedding_dim
+        self.patch_embedding = PatchEmbedding(patch_size=patch_size,max_length=max_length, emb_dim=self.embedding_dim,img_channels=img_channels)
         self.embedding_dropout = nn.Dropout(p=embedding_dropout)
-        self.positional_embedding = PositionalEmbedding(emb_dim=embedding_dim,max_length=max_length)
-
+        self.positional_embedding = PositionalEmbedding(emb_dim=self.embedding_dim,max_length=max_length)
         self.transformer_encoder = nn.TransformerEncoder( 
-            nn.TransformerEncoderLayer(d_model=embedding_dim, nhead=num_heads, dim_feedforward=mlp_size, dropout=attn_dropout),
+            nn.TransformerEncoderLayer(d_model=self.embedding_dim, nhead=num_heads, dim_feedforward=mlp_size, dropout=attn_dropout),
             num_layers=num_transformer_layers)
 
             # 10. Create classifier head
         self.classifier = nn.Sequential(
-            nn.LayerNorm(normalized_shape=embedding_dim),
-            nn.Linear(in_features=embedding_dim, 
+            nn.LayerNorm(normalized_shape=self.embedding_dim),
+            nn.Linear(in_features=self.embedding_dim, 
                     out_features=num_classes)
                                     )
         
@@ -117,11 +128,11 @@ class ResoVit(nn.Module):
         for i, img in enumerate(x):
             #Add a batch dimension to img:
             img = img.unsqueeze(0)
-            patches, attn_mask = self.patch_embedding(img)
+            patches, attn_mask, x_pos, y_pos = self.patch_embedding(img)
             # Make sure the attn_mask is on the same device as the patches
             attn_mask = attn_mask.to(patches.device)
             patches = self.embedding_dropout(patches)
-            patches = self.positional_embedding(patches)
+            patches = self.positional_embedding(patches,x_pos,y_pos)
             if i == 0:
                 x = patches
                 attn_masks = attn_mask
@@ -132,5 +143,8 @@ class ResoVit(nn.Module):
         x = x.mean(dim=1) # Take the average of the embeddings across the sequence dimension
         x = self.classifier(x)
         return x
+    
+
+
 
 
